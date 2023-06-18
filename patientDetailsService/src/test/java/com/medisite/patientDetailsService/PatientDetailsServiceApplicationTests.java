bf5255b25c9dbab22c47f4ebab280ba7e0a89482
Lucie <lucie.fischer@outlook.com>
2023-06-18 14:15:36 +0200

    creating mono repo

---
+++ patientDetailsService/src/test/java/com/medisite/patientDetailsService/PatientDetailsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medisite.patientDetailsService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientDetailsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
